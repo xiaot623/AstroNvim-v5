43a1a7fe8fb1d168cb21135cf13eb92a22269bf1
xiaot <hi@xiaot.tech>
2026-06-08 01:52:33 +0800

    chore: remove github copilot

--- lua/plugins/themes/catppuccin.lua
+++ lua/plugins/themes/catppuccin.lua
@@ -5,4 +5,24 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   lazy = false,
+  opts = {
+    transparent_background = true,
+    integrations = {
+      native_lsp = {
+        enabled = true,
+      },
+    },
+    --   custom_highlights = function()
+    --     return {
+    --       Normal = { bg = "NONE" },
+    --       NormalNC = { bg = "NONE" },
+    --       NormalFloat = { bg = "NONE" },
+    --       FloatBorder = { bg = "NONE" },
+    --       SignColumn = { bg = "NONE" },
+    --       EndOfBuffer = { bg = "NONE" },
+    --       StatusLine = { bg = "NONE" },
+    --       StatusLineNC = { bg = "NONE" },
+    --     }
+    --   end,
+  },
 }
